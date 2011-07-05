2ecd7da507d34d1789505c5459ec839551e2cd11
Pablo Winant <pablo@platoon.(none)>
2011-07-05 23:20:28 +0200

    Corrected order of derivatives in optimal_growth_recs (see preprocessor)

--- demos/optimal_growth/optimal_growth_recs.m
+++ demos/optimal_growth/optimal_growth_recs.m
@@ -1,36 +1,35 @@
 
 function [out1,out2,out3,out4,out5,out6] = mf_optimal_growth(flag,s,x,z,e,snext,xnext,p,out);
 
-% p is the vector of parameters
 
-output = struct('F',1,'Js',0,'Jx',0,'Jsn',0,'Jxn',0,'Jz',0, 'hmult',0);
+    output = struct('F',1,'Js',0,'Jx',0,'Jsn',0,'Jxn',0,'Jz',0, 'hmult',0);
 
-if nargin == 9
-    output = catstruct(output,out);
-    voidcell                        = cell(1,6);
-    [out1,out2,out3,out4,out5,out6] = voidcell{:};
-else
-    if nargout >= 2
-        output.Js = 1;
-    end
-    if nargout >= 3
-        output.Jx = 1;
-    end
-    if nargout >= 4
-        if strcmpi(flag, 'f')
-            output.Jz = 1;
-        else
-            output.Jsn = 1;
+    if nargin == 9
+        output = catstruct(output,out);
+        voidcell                        = cell(1,6);
+        [out1,out2,out3,out4,out5,out6] = voidcell{:};
+    else
+        if nargout >= 2
+            output.Js = 1;
+        end
+        if nargout >= 3
+            output.Jx = 1;
+        end
+        if nargout >= 4
+            if strcmpi(flag, 'f')
+                output.Jz = 1;
+            else
+                output.Jsn = 1;
+            end
+        end
+        if nargout >= 5
+            output.Jxn = 1;
+        end
+        if nargout >= 6
+            output.hmult = 1;
         end
     end
-    if nargout >= 5
-        output.Jxn = 1;
-    end
-    if nargout >= 6
-        output.hmult = 1;
-    end
-end
-    
+
 
 switch flag
 
@@ -46,40 +45,39 @@ case 'b';
 
 case 'f';
 	n = size(s,1);
-    ep = z;
 
         % f
         if output.F
 		out1 = zeros(n,2);
-		out1(:,1) = -1 + (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*p(1).*p(4).*ep(:,1);
-		out1(:,2) = -x(:,2) + p(1).*ep(:,2) + x(:,1).^(1 - p(2))./(1 - p(2));
+		out1(:,1) = -1 + (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*p(1).*p(4).*z(:,1);
+		out1(:,2) = -x(:,2) + p(1).*z(:,2) + x(:,1).^(1 - p(2))./(1 - p(2));
 
         end
         % df/ds
         if output.Js
 		out2 = zeros(n,2,2);
-		out2(:,1,1) = (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*(1 - p(4)).*p(1).*p(4).*ep(:,1)./(-x(:,1) + s(:,2)) + (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*p(1).*p(2).*p(4).*ep(:,1)./x(:,1); % d eq_1 w.r.t. c
-		out2(:,1,2) = 0; % d eq_1 w.r.t. Welf
-		out2(:,2,1) = x(:,1).^(1 - p(2))./x(:,1); % d eq_2 w.r.t. c
-		out2(:,2,2) = -1; % d eq_2 w.r.t. Welf
+		out2(:,1,1) = 0; % d eq_1 w.r.t. A
+		out2(:,1,2) = -(-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*(1 - p(4)).*p(1).*p(4).*z(:,1)./(-x(:,1) + s(:,2)); % d eq_1 w.r.t. w
+		out2(:,2,1) = 0; % d eq_2 w.r.t. A
+		out2(:,2,2) = 0; % d eq_2 w.r.t. w
 
         end
         % df/dx
         if output.Jx
 		out3 = zeros(n,2,2);
-		out3(:,1,1) = (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*p(1).*p(4); % d eq_1 w.r.t. eh
-		out3(:,1,2) = 0; % d eq_1 w.r.t. eW
-		out3(:,2,1) = 0; % d eq_2 w.r.t. eh
-		out3(:,2,2) = p(1); % d eq_2 w.r.t. eW
+		out3(:,1,1) = (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*(1 - p(4)).*p(1).*p(4).*z(:,1)./(-x(:,1) + s(:,2)) + (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*p(1).*p(2).*p(4).*z(:,1)./x(:,1); % d eq_1 w.r.t. c
+		out3(:,1,2) = 0; % d eq_1 w.r.t. Welf
+		out3(:,2,1) = x(:,1).^(1 - p(2))./x(:,1); % d eq_2 w.r.t. c
+		out3(:,2,2) = -1; % d eq_2 w.r.t. Welf
 
         end
         % df/dz
         if output.Jz
 		out4 = zeros(n,2,2);
-		out4(:,1,1) = 0; % d eq_1 w.r.t. A
-		out4(:,1,2) = -(-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*(1 - p(4)).*p(1).*p(4).*ep(:,1)./(-x(:,1) + s(:,2)); % d eq_1 w.r.t. w
-		out4(:,2,1) = 0; % d eq_2 w.r.t. A
-		out4(:,2,2) = 0; % d eq_2 w.r.t. w
+		out4(:,1,1) = (-x(:,1) + s(:,2)).^(-1 + p(4)).*x(:,1).^p(2).*p(1).*p(4); % d eq_1 w.r.t. eh
+		out4(:,1,2) = 0; % d eq_1 w.r.t. eW
+		out4(:,2,1) = 0; % d eq_2 w.r.t. eh
+		out4(:,2,2) = p(1); % d eq_2 w.r.t. eW
 
         end
         
@@ -96,18 +94,18 @@ case 'g';
         end
         if output.Js
 		out2 = zeros(n,2,2);
-		out2(:,1,1) = 0; % d eq_1 w.r.t. c
-		out2(:,1,2) = 0; % d eq_1 w.r.t. Welf
-		out2(:,2,1) = -(-x(:,1) + s(:,2)).^p(4).*(1 - (1 - s(:,1)).*p(3) + e(:,1)).*p(4)./(-x(:,1) + s(:,2)); % d eq_2 w.r.t. c
-		out2(:,2,2) = 0; % d eq_2 w.r.t. Welf
+		out2(:,1,1) = p(3); % d eq_1 w.r.t. A
+		out2(:,1,2) = 0; % d eq_1 w.r.t. w
+		out2(:,2,1) = (-x(:,1) + s(:,2)).^p(4).*p(3); % d eq_2 w.r.t. A
+		out2(:,2,2) = (-x(:,1) + s(:,2)).^p(4).*(1 - (1 - s(:,1)).*p(3) + e(:,1)).*p(4)./(-x(:,1) + s(:,2)); % d eq_2 w.r.t. w
 
         end
         if output.Jx
 		out3 = zeros(n,2,2);
-		out3(:,1,1) = p(3); % d eq_1 w.r.t. A
-		out3(:,1,2) = 0; % d eq_1 w.r.t. w
-		out3(:,2,1) = (-x(:,1) + s(:,2)).^p(4).*p(3); % d eq_2 w.r.t. A
-		out3(:,2,2) = (-x(:,1) + s(:,2)).^p(4).*(1 - (1 - s(:,1)).*p(3) + e(:,1)).*p(4)./(-x(:,1) + s(:,2)); % d eq_2 w.r.t. w
+		out3(:,1,1) = 0; % d eq_1 w.r.t. c
+		out3(:,1,2) = 0; % d eq_1 w.r.t. Welf
+		out3(:,2,1) = -(-x(:,1) + s(:,2)).^p(4).*(1 - (1 - s(:,1)).*p(3) + e(:,1)).*p(4)./(-x(:,1) + s(:,2)); % d eq_2 w.r.t. c
+		out3(:,2,2) = 0; % d eq_2 w.r.t. Welf
 
         end
         
@@ -155,8 +153,7 @@ case 'h';
 
         end
         if output.hmult
-            out6 = ones(size(out1));
-            %warning('hmult not implemented')
+		out6 = ones(size(e,1),size(x,2));
         end
         
 
