4de43d353bd1db891d4640466f3e6125f0942a17
Christophe Gouel <christophe.gouel@grignon.inra.fr>
2011-08-24 12:25:38 +0200

    Introduce a function a launch dolo executable from recs

--- recsInstall.m
+++ recsInstall.m
@@ -3,17 +3,17 @@
 % Copyright (C) 2011 Christophe Gouel
 % Licensed under the Expat license, see LICENSE.txt
 
-dsemdirectory     = strrep(which('recsSimul'),'recsSimul.m','');
+recsdirectory     = strrep(which('recsSimul'),'recsSimul.m','');
 compecondirectory = strrep(which('remsolve.m'),'remsolve.m','');
 
 if ispc
-  strarray = ['copy ' compecondirectory 'private\arraymult.mex* ' dsemdirectory]
+  strarray = ['copy ' compecondirectory 'private\arraymult.mex* ' recsdirectory]
   dos(strarray);
-  strarray = ['copy ' compecondirectory         'arraymult.mex* ' dsemdirectory]
+  strarray = ['copy ' compecondirectory         'arraymult.mex* ' recsdirectory]
   dos(strarray);
 else % unix or mac
-  strarray = ['cp ' compecondirectory 'private/arraymult.mex* ' dsemdirectory]
+  strarray = ['cp ' compecondirectory 'private/arraymult.mex* ' recsdirectory]
   unix(strarray);
-  strarray = ['cp ' compecondirectory         'arraymult.mex* ' dsemdirectory];
+  strarray = ['cp ' compecondirectory         'arraymult.mex* ' recsdirectory];
   unix(strarray);
 end
