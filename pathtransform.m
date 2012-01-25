23402a5767b4c70350efe747bd86d7d55139727c
Christophe Gouel <cgouel@worldbank.org>
2012-01-25 15:37:39 -0500

    Correct bugs in domain error calculation

--- pathtransform.m
+++ pathtransform.m
@@ -4,13 +4,12 @@ function [F,J,domerr] = pathtransform(x,jacflag)
 % Path does not accept anonymous function or supplementary arguments, so model
 % parameters have to be passed in a global variable
 
-% Copyright (C) 2011 Christophe Gouel
+% Copyright (C) 2011-2012 Christophe Gouel
 % Licensed under the Expat license, see LICENSE.txt
 
 global par
 model = par{1};
 
-domerr = 0;
 if (jacflag)
   [F,J] = model(x,par{2:end});
 else
@@ -18,6 +17,14 @@ else
   J     = [];
 end
 
-% In test:
-isdomerr = ~(isfinite(F)+(imag(F)==0));
-if sum(isdomerr)>=1, fprintf(1,'domerr = %i \n',sum(isdomerr)); end
+%%  Calculation of number of domain errors
+domerr = 0;
+% Domain errors should be calculated from the situation when x values exceed
+% domain definition of the functions used. Instead, for simplicity, here the
+% number of domain errors is calculated from the output: the number of infinite,
+% complex or NaN numbers in F:
+isdomerr = ~isfinite(F) | (imag(F)~=0) | isnan(F);
+if any(isdomerr)
+%  fprintf(1,'domerr = %i \n',sum(isdomerr));
+  domerr = sum(isdomerr);
+end
