6b4047764c51f2dd3569988a11ace7146635a635
Christophe Gouel <christophe.gouel@grignon.inra.fr>
2012-09-12 14:20:22 +0200

    Add an option field to recsmodelinit.m to suppress outpout and pass options to recsSS.m

--- recsmodelinit.m
+++ recsmodelinit.m
@@ -1,9 +1,9 @@
-function model = recsmodelinit(inputfile,shocks,outputfile)
+function model = recsmodelinit(inputfile,shocks,outputfile,options)
 % RECSMODELINIT Prepares a RECS model structure
 %
-% RECSMODELINIT uses dolo (https://github.com/albop/dynare-python), a python
-% preprocessor, to convert the model described in a yaml file to a file readable by
-% MATLAB and RECS programs. In the conversion, dolo calculates the analytic
+% RECSMODELINIT uses dolo (https://github.com/albop/dolo), a python
+% preprocessor, to convert the model described in a yaml file to a file readable
+% by MATLAB and RECS programs. In the conversion, dolo calculates the analytic
 % representation of all partial derivatives.
 %
 % RECSMODELINIT(INPUTFILE) converts a model structure file, indicated by the string
@@ -26,11 +26,32 @@ function model = recsmodelinit(inputfile,shocks,outputfile)
 %
 % MODEL = RECSMODELINIT(INPUTFILE,SHOCKS,OUTPUTFILE) uses the string OUTPUTFILE
 % to name the m-file containing the model.
+%
+% MODEL = RECSMODELINIT(INPUTFILE,SHOCKS,OUTPUTFILE,OPTIONS) uses the options
+% defined by the structure OPTIONS. The fields of the structure are
+%    display          : 1 to display the steady state if found (default: 1)
+%    eqsolver         : solver used to find the steady state 'fsolve', 'lmmcp'
+%                       (default), 'ncpsolve' or 'path'
+%    eqsolveroptions  : options structure to be passed to eqsolver
 
 % Copyright (C) 2011-2012 Christophe Gouel
 % Licensed under the Expat license, see LICENSE.txt
 
-if nargin<3, outputfile = strrep(inputfile,'.yaml','model.m'); end
+%% Initialization
+if nargin<3 || isempty(outputfile)
+  outputfile = strrep(inputfile,'.yaml','model.m');
+end
+
+defaultopt = struct(                           ...
+    'display'           , 1                   ,...
+    'eqsolver'          , 'lmmcp'             ,...
+    'eqsolveroptions'   , struct([]));
+if nargin<4
+  options = defaultopt;
+else
+  warning('off','catstruct:DuplicatesFound')
+  options = catstruct(defaultopt,options);
+end
 
 %% Run dolo-recs on the yaml file
 recsdirectory = fileparts(which('recsSimul'));
@@ -58,7 +79,7 @@ end
 model.func        = eval(['@' strrep(outputfile,'.m','')]);
 model.params      = model.func('params');
 
-%% Prepare shocks information
+%% Prepare shocks information & find steady state
 if nargin>=2 && ~isempty(shocks)
   % Unpack shocks
   Mu    = shocks.Mu;
@@ -83,8 +104,8 @@ if nargin>=2 && ~isempty(shocks)
   %% Check steady state
   [sss0,xss0] = model.func('ss');
   if ~isempty(sss0) && ~isempty(xss0)
-    [sss,xss,zss,exitflag] = recsSS(model,sss0,xss0);
-    if exitflag==1
+    [sss,xss,zss,exitflag] = recsSS(model,sss0,xss0,options);
+    if exitflag==1 && options.display==1
       disp('Deterministic steady state')
       fprintf(1,' State variables:\n\t\t')
       fprintf(1,'%0.4g\t',sss)
