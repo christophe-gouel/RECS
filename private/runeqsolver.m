015fb0025427c540a258173c9424acc03733e37b
Christophe Gouel <christophe.gouel@grignon.inra.fr>
2012-09-19 10:53:46 +0200

    Introduce a derivative check option to facilite test of new developments

--- private/runeqsolver.m
+++ private/runeqsolver.m
@@ -7,7 +7,16 @@ function [x,f,exitflag] = runeqsolver(func,x,LB,UB,eqsolver,eqsolveroptions,vara
 % Copyright (C) 2011-2012 Christophe Gouel
 % Licensed under the Expat license, see LICENSE.txt
 
-%%  
+%% Derivative Check
+if strcmp(eqsolveroptions.DerivativeCheck,'on')
+  Jnum  = numjac(func,x,[],varargin{:});
+  [~,J] = func(x,varargin{:});
+  dJ    = norm(full(abs(J-Jnum)./max(1.0,abs(J))),Inf);
+  fprintf(1,'Derivative Check: max relative diff = %g\n\n',dJ)
+  eqsolveroptions.DerivativeCheck = 'off';
+end
+
+%% Solve equations
 switch eqsolver
   case 'fsolve'
     options = optimset('Display','off',...
