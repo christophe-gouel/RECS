926fbf264d06e111067c865ba9452d5e97773f77
Christophe Gouel <christophe.gouel@grignon.inra.fr>
2012-11-13 14:42:31 +0100

    Correct Arhive.bat

--- Archive.bat
+++ Archive.bat
@@ -6,17 +6,21 @@ md recs-archive\html
 md recs-archive\demos
 md recs-archive\Python
 
-REM matlab -wait -nosplash -nojvm -noFigureWindows -r html/Publish_recs_help.m
+cd html
+matlab -wait -nosplash -r startup;Publish_recs_help;exit
+cd ..
+
+Markdown.pl README.md > README.html
 
 copy /Y . recs-archive
 copy /Y private recs-archive\private
 robocopy html recs-archive\html /E
 copy /Y demos recs-archive\demos
 robocopy Python recs-archive\Python /E
-copy /Y Archive\README.txt recs-archive
 
 cd recs-archive
 del .gitignore
+del .gitattributes
 del /S logfile.tmp 
 del Archive.bat
 del README.md
