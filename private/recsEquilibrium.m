c8b4e4809444e16442edf126965d416eb5b57625
Christophe Gouel <christophe.gouel@grignon.inra.fr>
2016-11-09 15:56:33 +0100

    Remove the need for a sparse block-diagonal matrix when loop_over_s=1

--- private/recsEquilibrium.m
+++ private/recsEquilibrium.m
@@ -10,9 +10,10 @@ function [F,Jx,Jc] = recsEquilibrium(x,s,z,b,f,g,h,params,gridJx,c,e,w,fspace,fu
 % Licensed under the Expat license, see LICENSE.txt
 
 %% Initialization
-[n,d] = size(s);
+n     = size(s,1);
 if ~ArrayProblem, x = reshape(x,[],n)'; end
 m     = size(x,2);
+mnext = length(ixforward); % Number of next-period response variables
 mf    = sum(ixforward); % Number of forward response variables
 
 %% Evaluate the equilibrium equations and Jacobian
@@ -36,6 +37,7 @@ switch funapprox
     if nargout>=2
       %% With Jacobians
       [snext,~,gx]        = g(ss,xx,ee,params,[1 0 1 0]);
+      d = size(snext,2);
       if extrapolate>=1, snextinterp = snext;
       else
           snextinterp = max(min(snext,fspace.b(ones(n*k,1),:)),fspace.a(ones(n*k,1),:));
@@ -54,7 +56,7 @@ switch funapprox
           [LBnext,UBnext]      = b(snext,params);
           Xnext                = funeval(c,fspace,Bsnext,...
                                          [zeros(1,d); eye(d)]);
-          xnext                = zeros(n*k,m);
+          xnext                = zeros(n*k,mnext);
           xnext(:,ixforward)   = min(max(Xnext(:,:,1),LBnext(:,ixforward)),...
                                      UBnext(:,ixforward));
           xfnextds             = Xnext(:,:,2:end);
@@ -142,7 +144,7 @@ switch funapprox
             snextinterp = max(min(snext,fspace.b(ones(n*k,1),:)), ...
                               fspace.a(ones(n*k,1),:));
           end
-          xnext              = zeros(n*k,m);
+          xnext              = zeros(n*k,mnext);
           xnext(:,ixforward) = min(max(funeval(c,fspace,snextinterp),...
                                        LBnext(:,ixforward)),UBnext(:,ixforward));
           hv                 = h(ss,xx,ee,snext,xnext,params);
@@ -158,6 +160,6 @@ if ~ArrayProblem
   F = reshape(F',n*m,1);
   if nargout>=2
     Jx = permute(Jx,[2 3 1]);
-    Jx = spblkdiag(Jx,gridJx);
+    if n>1, Jx = spblkdiag(Jx,gridJx); end
   end
 end
